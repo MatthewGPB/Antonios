29bb585ab20beac3f16fbc8841695a716d0bba8d
MatthewGPB <matthew@growpalmbeach.com>
2026-06-29 14:42:47 -0400

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.antoniositalianmarket.net',
+  site: 'https://antoniospb.com',
   output: 'static',
   integrations: [
     tailwind({
